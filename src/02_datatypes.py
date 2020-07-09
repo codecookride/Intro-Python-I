e02329c5f0575e30492fd65215ddfdf038fad169
codecookride <57050292+codecookride@users.noreply.github.com>
2020-07-08 21:04:34 -0700

    half way home:

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,10 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
-
+result = int(x) + int(y)
+print(result)
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+result2 = int(x) + int(y)
+print(str(result2))
